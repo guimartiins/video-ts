0ba362d90b0591c9c3da27f3119d61c04a74cfeb
Guilherme Martins <academicgmag@gmaiil.com>
2024-08-11 13:59:21 -0300

    creating in-memory repository for testing

--- src/shared/domain/repository/repository-interface.ts
+++ src/shared/domain/repository/repository-interface.ts
@@ -5,9 +5,9 @@ export interface IRepository<E extends Entity, EntityId extends ValueObject> {
     insert(entity: E): Promise<void>
     bulkInsert(entities: E[]): Promise<void>
     update(entity: E): Promise<void>
-    delete(entity: E): Promise<void>
+    delete(entity_id: EntityId): Promise<void>
 
-    findById(id: EntityId): Promise<E>
+    findById(id: EntityId): Promise<E | null>
     findAll(): Promise<E[]>
 
     getEntity(): new (...args: any[]) => E
