70000e153c6490685658b69af90b50a2ad4b546c
Guilherme Martins <academicgmag@gmaiil.com>
2024-11-25 10:17:52 +0000

    add controller test

--- src/categories/categories.controller.spec.ts
+++ src/categories/categories.controller.spec.ts
@@ -1,12 +1,32 @@
 import { Test, TestingModule } from '@nestjs/testing';
 import { CategoriesController } from './categories.controller';
+import { CategorySequelizeRepository } from '@core/category/domain/infra/db/sequelize/category-sequelize.repository';
+import { CategoryModel } from '@core/category/domain/infra/db/sequelize/category.model';
+import { getModelToken, SequelizeModule } from '@nestjs/sequelize';
 
 describe('CategoriesController', () => {
   let controller: CategoriesController;
 
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
+      imports: [
+        SequelizeModule.forRoot({
+          dialect: 'sqlite',
+          host: ':memory',
+          logging: false,
+          models: [CategoryModel]
+        }),
+        SequelizeModule.forFeature([CategoryModel])
+      ],
       controllers: [CategoriesController],
+      providers: [
+        {
+          provide: CategorySequelizeRepository,
+          useFactory: (categoryModel: typeof CategoryModel) => new CategorySequelizeRepository(categoryModel),
+          // inject the CategoryModel
+          inject: [getModelToken(CategoryModel)]
+        }
+      ]
     }).compile();
 
     controller = module.get<CategoriesController>(CategoriesController);
