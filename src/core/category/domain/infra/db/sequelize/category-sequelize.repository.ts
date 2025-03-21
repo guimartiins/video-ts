af8b555e5b01897fdc0b8d6c3ace0b57ee94f4aa
Guilherme Martins <academicgmag@gmaiil.com>
2025-03-21 08:24:57 -0300

    list e2e tests

--- src/core/category/domain/infra/db/sequelize/category-sequelize.repository.ts
+++ src/core/category/domain/infra/db/sequelize/category-sequelize.repository.ts
@@ -1,4 +1,4 @@
-import { Op } from 'sequelize'
+import { literal, Op } from 'sequelize'
 import { NotFoundError } from '../../../../../shared/domain/errors/not-found.error'
 import { UUID } from '../../../../../shared/domain/value-objects/uuid.vo'
 import { Category } from '../../../category.entity'
@@ -9,9 +9,15 @@ import {
 } from '../../../category.repository'
 import { CategoryModel } from './category.model'
 import { CategoryModelMapper } from './category-model-mapper'
+import { SortDirection } from '@core/shared/domain/repository/search-params'
 
 export class CategorySequelizeRepository implements ICategoryRepository {
   sortableFields: string[] = ['name', 'created_at']
+  orderBy = {
+    mysql: {
+      name: (sort_dir: SortDirection) => literal(`binary name ${sort_dir}`)
+    }
+  }
 
   constructor(private categoryModel: typeof CategoryModel) {}
 
@@ -86,7 +92,7 @@ export class CategorySequelizeRepository implements ICategoryRepository {
         }
       }),
       ...(props.sort && this.sortableFields.includes(props.sort)
-        ? { order: [[props.sort, props.sort_dir]] }
+        ? { order: this.formatSort(props.sort, props.sort_dir) }
         : { order: [['created_at', 'desc']] }),
       offset,
       limit
@@ -99,5 +105,13 @@ export class CategorySequelizeRepository implements ICategoryRepository {
       per_page: props.per_page,
       total: count
     })
+  }
+
+  private formatSort(sort: string, sort_dir: SortDirection) {
+    const dialect = this.categoryModel.sequelize.getDialect() as 'mysql'
+    if (this.orderBy[dialect] && this.orderBy[dialect][sort]) {
+      return this.orderBy[dialect][sort](sort_dir)
+    }
+    return [[sort, sort_dir]]
   }
 }
